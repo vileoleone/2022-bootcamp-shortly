239a95dde3cf8b9df2e7345f27d8057a868a2d23
Victor Leone de Oliveira <vileoleone@gmail.com>
2022-12-21 16:33:53 -0300

    feat: ranking bug fix: not showing users with 0 links

--- src/controllers/rankings.controllers.js
+++ src/controllers/rankings.controllers.js
@@ -2,7 +2,7 @@ import { connectionWithDB } from "../database/db.js"
 
 export async function rankingUrls (req, res) {
     try {
-        const ranking = await connectionWithDB.query(`SELECT users.id, users.name, COUNT(urls.url) AS "linksCount", SUM(urls.visits) AS "visitCount" FROM users JOIN Urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10;`)
+        const ranking = await connectionWithDB.query(`SELECT users.id, users.name, COUNT(urls.url) AS "linksCount", SUM(urls.visits) AS "visitCount" FROM users LEFT JOIN Urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10;`)
 
         res.status(200).send(ranking.rows)
         
