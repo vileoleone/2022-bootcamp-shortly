6dcd8fec7a9be030efe70f88aac78ba5603e56e0
Victor Leone de Oliveira <vileoleone@gmail.com>
2022-12-21 17:29:06 -0300

    feat: fixing ranking null values

--- src/controllers/rankings.controllers.js
+++ src/controllers/rankings.controllers.js
@@ -1,19 +1,22 @@
 import { connectionWithDB } from "../database/db.js"
 
-export async function rankingUrls (req, res) {
+export async function rankingUrls(req, res) {
     try {
         console.log("oi")
         const ranking = await connectionWithDB.query(`SELECT users.id, users.name, COUNT(urls.url) AS "linksCount", SUM(urls.visits) AS "visitCount" FROM users LEFT JOIN Urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10;`)
-        
-        /* const rankingsFormat = ranking.rows.forEach(obj => {
-            if (obj.visitCount == null) {
+
+        const rankingFormat = ranking.rows
+
+        rankingFormat.forEach(obj => {
+            visitCount = obj.visitCount
+            if (visitCount == null) {
                 obj.visitCount = 0
             }
-        }); */
-        res.status(200).send(ranking.rows)
-        
+        });
+        res.status(200).send(rankingFormat)
+
     } catch (err) {
-       res.status(500).send(err.message) 
+        res.status(500).send(err.message)
     }
 }
 
