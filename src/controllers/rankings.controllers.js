23e1967d34b8e9905e683487c0be63beec4f493d
Victor Leone de Oliveira <vileoleone@gmail.com>
2022-12-21 16:41:04 -0300

    feat: correcting null values in /rankings

--- src/controllers/rankings.controllers.js
+++ src/controllers/rankings.controllers.js
@@ -2,9 +2,17 @@ import { connectionWithDB } from "../database/db.js"
 
 export async function rankingUrls (req, res) {
     try {
+        console.log("oi")
         const ranking = await connectionWithDB.query(`SELECT users.id, users.name, COUNT(urls.url) AS "linksCount", SUM(urls.visits) AS "visitCount" FROM users LEFT JOIN Urls ON users.id = urls."userId" GROUP BY users.id ORDER BY "visitCount" DESC LIMIT 10;`)
-
-        res.status(200).send(ranking.rows)
+        
+        const rankingsFormat = ranking.rows.forEach(obj => {
+            console.log
+            if (obj.visitCount == null) {
+                obj.visitCount = 0
+            }
+        });
+        console.log(ranking)
+        res.status(200).send(rankingsFormat)
         
     } catch (err) {
        res.status(500).send(err.message) 
